e4523682d9e083af8b7ee26e80937deea9f4884c
xiaolei1998 <xiaolei336699@gmail.com>
2022-11-03 17:02:33 +0800

    setting up camera
    camera ok

---
+++ image_acquisition.py
@@ -0,0 +1,31 @@
+import cv2
+from time import sleep
+import sys
+import signal
+
+"""
+get dependent package and lib:
+1. $ brew install opencv
+2. $ pip3 install opencv-python
+"""
+
+def handler(signum, frame):
+    print("forced shut down")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
+
+#setting up the camera port
+cam = cv2.VideoCapture(0)
+
+while(True):
+    #acquisit real-time images
+    captured, image = cam.read()
+
+    if captured:
+        #save the input to local path
+        cv2.imwrite("input_img.png", image)
+
+    sleep(5)
+
+
